da43a0c125e8ef22bcf869c50b947ddf24b8e842
awalvie <agrawal.vishesh.178@gmail.com>
2019-06-19 18:19:20 +0530

    Week 2 and Week 3

--- _posts/generator.py
+++ _posts/generator.py
@@ -14,12 +14,13 @@ title: "{0}"
 layout: post
 date: {1}
 tag:
-- 
+- 100-days-of-code
 star: true
 category: blog
 author: awalvie 
 description: "{2}"
 ---
+<div class="breaker"></div> <a id="personalize"></a>
 '''.format(title, time, description) 
 
 print(text_to_write)
